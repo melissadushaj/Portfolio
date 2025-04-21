5b03fd963af26c4fefd64c55e21f7209032b28b0
jynxedd <melissadushaj@gmail.com>
2025-04-21 18:42:57 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function App() {
+  
+  return (
+    <div 
+      style = {{
+        backgroundImage: "url(Background.png)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <h1>HELLOOOOOO</h1>
+    </div>
+  )
+}
